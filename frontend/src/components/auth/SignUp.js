0ae2a5623eeed380b60e9075ed6a6adca4515c02
Rana Ryahan <ryanrazz00@gmail.com>
2024-10-12 12:13:17 +0300

    Final client

--- frontend/src/components/auth/SignUp.js
+++ frontend/src/components/auth/SignUp.js
@@ -11,19 +11,22 @@ const Signup = () => {
     password: "",
     phone: "",
   });
+  const [isSigningUp, setIsSigningUp] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Handle form submission
+    setIsSigningUp(true);
     try {
       // Simulating an API call
       await axios.post("/api/users/process-register", formData);
-      toast.success("Registration successfull. Please login!");
+      toast.success("Registration successful. Please login!");
       // Clear the form
       setFormData({ name: "", email: "", password: "", phone: "" });
     } catch (error) {
       console.log(error);
       toast.error(error?.response?.data?.message);
+    } finally {
+      setIsSigningUp(false);
     }
   };
 
@@ -48,7 +51,7 @@ const Signup = () => {
               <input
                 id="full-name"
                 name="fullname"
-                type="name"
+                type="text"
                 autoComplete="name"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-700 placeholder-gray-500 text-gray-200 rounded-r-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm bg-gray-800"
@@ -77,7 +80,6 @@ const Signup = () => {
                 }
               />
             </div>
-
             <div className="flex items-center">
               <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-700 bg-gray-800 text-gray-500 text-sm">
                 <Lock className="h-5 w-5" />
@@ -86,7 +88,7 @@ const Signup = () => {
                 id="password"
                 name="password"
                 type="password"
-                autoComplete="current-password"
+                autoComplete="new-password"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-700 placeholder-gray-500 text-gray-200 rounded-r-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm bg-gray-800"
                 placeholder="Password"
@@ -103,8 +105,8 @@ const Signup = () => {
               <input
                 id="phone"
                 name="phone"
-                type="text"
-                autoComplete="phone"
+                type="tel"
+                autoComplete="tel"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-700 placeholder-gray-500 text-gray-200 rounded-r-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm bg-gray-800"
                 placeholder="Phone"
@@ -145,16 +147,49 @@ const Signup = () => {
           <div>
             <button
               type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white ${
+                isSigningUp
+                  ? "bg-indigo-400 cursor-not-allowed"
+                  : "bg-indigo-600 hover:bg-indigo-700"
+              } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+              disabled={isSigningUp}
             >
-              Sign in
-              <LogIn className="ml-2 h-5 w-5" />
+              {isSigningUp ? (
+                <>
+                  Signing up...
+                  <svg
+                    className="animate-spin ml-2 h-5 w-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                </>
+              ) : (
+                <>
+                  Sign up
+                  <LogIn className="ml-2 h-5 w-5" />
+                </>
+              )}
             </button>
           </div>
         </form>
         <div className="text-center">
           <p className="text-sm text-gray-400">
-            Don't have an account?{" "}
+            Already have an account?{" "}
             <Link
               to="/login"
               className="font-medium text-indigo-400 hover:text-indigo-500"
